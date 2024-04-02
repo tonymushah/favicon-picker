1436fea1fbb848d8daab06c805866208cd15a1ea
tonymushah <tonyramanantsalama@gmail.com>
2024-04-02 18:16:41 +0300

    Finished Lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod error;
+pub(crate) mod favicon;
+mod icons_from_html;
+pub mod selectors;
+
+pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
+
+use error::Error;
+use favicon::InnerFavicon;
+pub use icons_from_html::*;
+
+pub use favicon::Favicon;
+use scraper::Html;
+use url::Url;
+
+pub fn get_default_favicon_url(base_url: &Url) -> std::result::Result<Url, url::ParseError> {
+    base_url.join("/favicon.ico")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn get_favicons_from_url(
+    client: &reqwest::Client,
+    base_url: &Url,
+) -> Result<Vec<Favicon>> {
+    let html_raw = client.get(base_url.clone()).send().await?.text().await?;
+    let html = Html::parse_document(&html_raw);
+    let icons = InnerFavicon::extract_favicons(&html)?
+        .into_iter()
+        .flat_map(|e| <Favicon as TryFrom<(&Url, InnerFavicon<'_>)>>::try_from((base_url, e)))
+        .collect::<Vec<Favicon>>();
+    if icons.is_empty() {
+        Err(Error::NoLinkIconElements)
+    } else {
+        Ok(icons)
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg(feature = "blocking")]
+pub fn get_blocking_favicons_from_url(
+    client: &reqwest::blocking::Client,
+    base_url: &Url,
+) -> Result<Vec<Favicon>> {
+    let html_raw = client.get(base_url.clone()).send()?.text()?;
+    let html = Html::parse_document(&html_raw);
+    let icons = InnerFavicon::extract_favicons(&html)?
+        .into_iter()
+        .flat_map(|e| <Favicon as TryFrom<(&Url, InnerFavicon<'_>)>>::try_from((base_url, e)))
+        .collect::<Vec<Favicon>>();
+    if icons.is_empty() {
+        Err(Error::NoLinkIconElements)
+    } else {
+        Ok(icons)
     }
 }
