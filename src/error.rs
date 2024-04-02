e6f188790325c833c6c5d2f5504ffd652b4f18c4
tonymushah <tonyramanantsalama@gmail.com>
2024-04-02 18:05:51 +0300

    Added `Reqwest` to `crate::error::Error`

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,13 @@
 use scraper::error;
 
-#[derive(Debug, thiserror::Error, Clone)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("{0}")]
     SelectorKind(#[from] error::SelectorErrorKind<'static>),
     #[error("{0}")]
     UrlParse(#[from] url::ParseError),
     #[error("the `href` attribute is not found in element_ref")]
-    HrefNotFound
+    HrefNotFound,
+    #[error(transparent)]
+    Reqwest(#[from] reqwest::Error)
 }
