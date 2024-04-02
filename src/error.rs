079c26605f7cbbea0ab059d832c6cd8a3f515cd9
tonymushah <tonyramanantsalama@gmail.com>
2024-04-02 13:31:37 +0300

    Added `Clone` derive to `crate::error::Error`

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use scraper::error;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, Clone)]
 pub enum Error {
-    #[error(transparent)]
+    #[error("{0}")]
     SelectorKind(#[from] error::SelectorErrorKind<'static>)
 }
