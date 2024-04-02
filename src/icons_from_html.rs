f74aa4d02ddba1380ee831bc104e897cc649f34c
tonymushah <tonyramanantsalama@gmail.com>
2024-04-02 13:25:03 +0300

    Added `extract_icon_links_from_html`

---
+++ src/icons_from_html.rs
@@ -0,0 +1,32 @@
+use scraper::{ElementRef, Html};
+
+use crate::{selectors::favicon_selector, Result};
+
+pub fn extract_icon_links_from_html(html: &Html) -> Result<Vec<ElementRef<'_>>> {
+    let selector = favicon_selector()?;
+    Ok(html.select(&selector).collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use scraper::Html;
+
+    use crate::extract_icon_links_from_html;
+
+    #[test]
+    fn extract_from_html() -> anyhow::Result<()> {
+        let html_raw = "
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <link rel='icon' size='96x96' href='/some-favicon.ico'/>
+                    <link rel='icon' size='any' href='/favicon.ico'/>
+                </head>
+            </html>
+        ";
+        let html = Html::parse_document(html_raw);
+        let icons = extract_icon_links_from_html(&html).map_err(|e| anyhow::Error::msg(e.to_string()))?;
+        assert_eq!(icons.len(), 2);
+        Ok(())
+    }
+}
