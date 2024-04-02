3192b1fe11c05e14dbf02b1859bdaa68eb7e991a
tonymushah <tonyramanantsalama@gmail.com>
2024-04-02 18:04:29 +0300

    Added `UrlParse` and `HrefNotFound` error

--- src/error.rs
+++ src/error.rs
@@ -3,5 +3,9 @@ use scraper::error;
 #[derive(Debug, thiserror::Error, Clone)]
 pub enum Error {
     #[error("{0}")]
-    SelectorKind(#[from] error::SelectorErrorKind<'static>)
+    SelectorKind(#[from] error::SelectorErrorKind<'static>),
+    #[error("{0}")]
+    UrlParse(#[from] url::ParseError),
+    #[error("the `href` attribute is not found in element_ref")]
+    HrefNotFound
 }
